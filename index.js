ec306a5867a0f8db9f7360f98f54c10219a22463
Kazuki-Tohyama <tonmas.thk@gmail.com>
2021-07-21 22:54:15 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+exports.handler = async (event) => {
+  let S3_ENDPOINT, DYNAMODB_ENDPOINT;
+
+  if (event['stage-variables']['env'] === 'prod') {
+    S3_ENDPOINT = process.env.PROD_S3_ARN;
+    DYNAMODB_ENDPOINT = process.env.PROD_DYNAMODB_ARN;
+  } else {
+    S3_ENDPOINT = process.env.DEV_S3_ARN;
+    DYNAMODB_ENDPOINT = process.env.DEV_DYNAMODB_ARN;
+  }
+
+  const responseBody = event['body-json'];
+
+  const response = {
+    statusCode: 200,
+    body: responseBody,
+  };
+  return response;
+};
